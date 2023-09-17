a4953cf986cb783102d21a31456e49733025e29e
John Geronimo <johnageronimo1@gmail.com>
2023-09-17 16:03:56 +1200

    About page

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,10 @@
+import { About } from "@/components/About/About"
+import Box from '@mui/material/Box'
+
+export const AboutPage = () => {
+  return (
+    <Box>
+      <About />
+    </Box>
+  )
+}
