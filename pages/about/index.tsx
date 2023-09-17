1b67c3179321aaf51fc2b922f0637efc559b97a0
John Geronimo <johnageronimo1@gmail.com>
2023-09-17 16:40:21 +1200

    restructured files

---
+++ pages/about/index.tsx
@@ -0,0 +1,11 @@
+import { About } from '../../src/components/About'
+import Box from '@mui/material/Box'
+import { ReactElement } from "react"
+
+export default function AboutPage(): ReactElement {
+  return (
+    <Box>
+      <About />
+    </Box>
+  )
+}
